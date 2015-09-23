e48027d7eedb64388e050ee98ce38afc6f5730b9
Torstein Meland <torstein.meland@gmail.com>
2015-09-23 21:10:01 +0200

    Fix code after review
    The overridden method onOptionsItemSelected() can be safely deleted
    because the app does not use it. It takes a lot of space and this
    reduction would make the code more compacted and therefore readable. It
    is also a distraction from the content that matters and we anticipate
    that this method won't be needed during the application development.
    
    Notice, that there is a lag when clicking immediately more than one
    button. A user has to wait for the second and subsequent toasts to be
    displayed. It is very simple to enhance, the method .cancel() makes a
    toast disappear (it's important to check whether the toast is null and
    to declare an appropriate member variable).

--- app/src/main/java/com/example/torsteinmeland/projectportfolio/MainActivity.java
+++ app/src/main/java/com/example/torsteinmeland/projectportfolio/MainActivity.java
@@ -1,22 +1,21 @@
 package com.example.torsteinmeland.projectportfolio;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toast currentToast= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -24,25 +23,14 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void sendMessage(View view) {
         final int resId= getToastIdFromButtonId(view.getId());
         final CharSequence text = getApplicationContext().getText(resId);
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+        if(currentToast!=null){
+            currentToast.cancel();
+        }
+        currentToast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        currentToast.show();
     }
 
     private int getToastIdFromButtonId(final int btnId) {
